939d07d5b9ea2f1eb86444fc3ef864d37587f706
joaomcordeiro <61886873+joaomcordeiro@users.noreply.github.com>
2022-06-18 22:41:16 +0100

    Update send.py

--- send.py
+++ send.py
@@ -2,7 +2,7 @@ import sendgrid
 import os
 from sendgrid.helpers.mail import *
 
-sg = sendgrid.SendGridAPIClient('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')
+sg = sendgrid.SendGridAPIClient('')
 from_email = Email("ispg4103@ispgaya.pt")
 to_email = To("joaomcordeiro98@gmail.com")
 subject = "Sending with SendGrid is Fun"
