2199f33c1f3d605133f99d97d4d0852241743b56
joaomcordeiro <61886873+joaomcordeiro@users.noreply.github.com>
2022-06-20 17:36:19 +0100

    Update send.py

--- send.py
+++ send.py
@@ -17,8 +17,40 @@ sender_password = "ispg12345"
 msg = MIMEMultipart()
 html_message = """
 <html>
-<bodystyle="background-color:#77b300;">
-<p style="font-family:verdana">Hi, it looks like your plant needs some water!</p>
+<head>
+<style>
+table {
+ 	 border-collapse: collapse;
+    padding-left: 10px;
+    width:100%;
+    font-family: 'Ascender Sans', sans-serif;
+    font-color: white;
+    font-size:11px;
+    text-align:center;
+    background-color:#77b300;
+}
+
+th, td {
+    padding:5px 10px;
+}
+
+th {
+    color:#77b300;
+    border-top:15px solid DarkOliveGreen;
+    border-bottom:25px solid black;
+    background-color:#77b300;
+}
+</style>
+</head>
+<body>
+<table>
+  <tr>
+  <th></th>
+  <th>Hi, it's time to water your plant!</th>
+  <th></th>
+  </tr>
+</table>
+
 </body>
 </html>
 """
